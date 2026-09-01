e879b8eb163d5320b5d7663f2b665a0483c9ecd8
nikhilss370 <nikhilss370@gmail.com>
2026-09-01 11:09:23 +0530

    Update RoomScene.jsx

--- src/engine/RoomScene.jsx
+++ src/engine/RoomScene.jsx
@@ -1,57 +1,39 @@
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { useState } from "react";
+import Wardrobe from "../components/Wardrobe";
 
-function Wardrobe(){
-
-  const [selected,setSelected]=useState(false);
-
-  return(
-    <group
-      position={[0,2,-3]}
-      onClick={()=>setSelected(!selected)}
-    >
-
-      <mesh>
-        <boxGeometry args={[2,4,0.6]}/>
-        <meshStandardMaterial color={selected?"#2563EB":"#8B5A2B"}/>
-      </mesh>
-
-      <mesh position={[-0.45,0,0.31]}>
-        <boxGeometry args={[0.9,3.9,0.02]}/>
-        <meshStandardMaterial color="#FFFFFF"/>
-      </mesh>
-
-      <mesh position={[0.45,0,0.31]}>
-        <boxGeometry args={[0.9,3.9,0.02]}/>
-        <meshStandardMaterial color="#FFFFFF"/>
-      </mesh>
-
-    </group>
+function Wall({position,rotation,size}) {
+  return (
+    <mesh position={position} rotation={rotation}>
+      <planeGeometry args={size}/>
+      <meshStandardMaterial color="#E7E2D6"/>
+    </mesh>
   );
 }
 
-export default function RoomScene(){
-
-  return(
+export default function RoomScene() {
+  return (
     <Canvas camera={{position:[8,6,8]}}>
-
       <ambientLight intensity={1}/>
-
       <directionalLight position={[5,8,5]}/>
 
+      {/* Floor */}
       <mesh rotation={[-Math.PI/2,0,0]}>
         <planeGeometry args={[12,10]}/>
         <meshStandardMaterial color="#8D7B68"/>
       </mesh>
 
+      {/* Walls */}
+      <Wall position={[0,4,-5]} rotation={[0,0,0]} size={[12,8]}/>
+      <Wall position={[-6,4,0]} rotation={[0,Math.PI/2,0]} size={[10,8]}/>
+      <Wall position={[6,4,0]} rotation={[0,-Math.PI/2,0]} size={[10,8]}/>
+
       <gridHelper args={[12,12]}/>
 
       <Wardrobe/>
 
-      <OrbitControls/>
-
+      <OrbitControls enableDamping/>
     </Canvas>
   );
 }
