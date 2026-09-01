1ecd901a74ce16fc04573b30ebd06f26df75a048
nikhilss370 <nikhilss370@gmail.com>
2026-09-01 11:11:02 +0530

    Update CabinetEngine.jsx

--- src/engine/CabinetEngine.jsx
+++ src/engine/CabinetEngine.jsx
@@ -1,1 +1,16 @@
 
+export const defaultWardrobe = {
+  width: 2000,
+  height: 2400,
+  depth: 600,
+  doors: 2,
+  shelves: 5,
+  material: "Walnut"
+};
+
+export function snapToWall(x,z){
+  if(x>5.5) x=5.5;
+  if(x<-5.5) x=-5.5;
+  if(z<-4.7) z=-4.7;
+  return {x,z};
+}
