d88178d58f3ac7a52cd7dae525a6d0699dc85bb4
nikhilss370 <nikhilss370@gmail.com>
2026-09-01 11:08:55 +0530

    Update Wardrobe.jsx

--- src/components/Wardrobe.jsx
+++ src/components/Wardrobe.jsx
@@ -1,1 +1,38 @@
 
+import { useState } from "react";
+
+export default function Wardrobe() {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <group position={[0,2,-3]} onClick={() => setSelected(!selected)}>
+      {/* Body */}
+      <mesh>
+        <boxGeometry args={[2,4,0.6]} />
+        <meshStandardMaterial color={selected ? "#2563EB" : "#8B5A2B"} />
+      </mesh>
+
+      {/* Left Door */}
+      <group
+        position={[-0.5,0,0.31]}
+        rotation={[0, open ? -1.2 : 0, 0]}
+        onClick={(e)=>{
+          e.stopPropagation();
+          setOpen(!open);
+        }}
+      >
+        <mesh position={[0.45,0,0]}>
+          <boxGeometry args={[0.9,3.9,0.02]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+      </group>
+
+      {/* Right Door */}
+      <mesh position={[0.45,0,0.31]}>
+        <boxGeometry args={[0.9,3.9,0.02]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+    </group>
+  );
+}
